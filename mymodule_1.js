01ef184fe64eb6ed2888847bf9033648b7d4dd44
fileszero <files.eq.zero@gmail.com>
2017-09-27 11:57:29 +0900

    change output string . more clear message.

--- mymodule_1.js
+++ mymodule_1.js
@@ -5,11 +5,11 @@
  * @returns void
  */
 var mymodule_1 = function(){
-    console.log("mymodule_1.constructor");
+    console.log("mymodule_1.constructor called.");
 };
 
 mymodule_1.prototype.method1=function(){
-    console.log("mymodule_1.method1");
+    console.log("mymodule_1.method1 called.");
 }
 
 module.exports=mymodule_1;
